8ad544bdcf028c93e3eb97401394da7b0f843dae
firdosneha <firdosneha563@gmail.com>
2026-03-08 15:48:10 +0530

    Update main.tf

--- ansible/main.tf
+++ ansible/main.tf
@@ -1,1 +1,15 @@
+- hosts: master
+  become: yes
 
+  tasks:
+
+  - name: install java
+    apt:
+      name: openjdk-17-jdk
+      state: present
+      update_cache: yes
+
+  - name: install jenkins
+    apt:
+      name: jenkins
+      state: present
